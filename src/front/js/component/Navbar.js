87f80cb8dfb3085a4952b26d83e59cdf708c5858
Michael Mirisciotta <michaelmirisciotta@gmail.com>
2025-06-06 18:45:57 -0700

    VibeCoding Email Signup  Verification9

--- src/front/js/component/Navbar.js
+++ src/front/js/component/Navbar.js
@@ -58,7 +58,7 @@ export const Navbar = () => {
 												<span className="navbarFont">Create Session</span>
 												<i className="fa-solid fa-folder-plus" />
 											</Link>
-											<Link to="/mentor-list" className="dropdown-item bold-text text-end">
+											<Link to="/mentors" className="dropdown-item bold-text text-end">
 												<span className="navbarFont">Find A Mentor</span>
 												<i className="fa-solid fa-folder-plus" />
 											</Link>
